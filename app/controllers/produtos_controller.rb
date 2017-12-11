fdcc3c0c9b8e0a9d97cb813267e7d80666adfe88
Felipe Rodrigues <felipe_rodriguesx@hotmail.com>
2017-12-10 23:47:29 -0200

    Search and delete features

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -3,4 +3,21 @@ class ProdutosController < ApplicationController
     @produtos_por_nome =  Produto.order(:nome).limit 5
     @produtos_por_preco = Produto.order(:preco).limit 2
   end
+
+  def busca
+    @nome_a_buscar = params[:nome]
+    @produtos = Produto.where "nome like ?", "%#{@nome_a_buscar}%"
+  end
+
+  def create
+    valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade
+    produto = Produto.create valores
+    redirect_to root_url
+  end
+
+  def destroy
+    id = params[:id]
+    Produto.destroy id
+    redirect_to root_url
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   #/path => controller#method
-  #get 'produtos' => 'produtos#index'
+  # get 'produtos' => 'produtos#index'
+  # post '/produtos' => 'produtos#create'
+  # get 'produtos/new' => 'produtos#new'
+  # delete 'produtos/:id' => 'produtos#destroy', as: :produto
+  #resources :produtos gera todas as rotas possíveis
+  resources :produtos, only: [:new, :create, :destroy]
+  get '/produtos/busca' => 'produtos#busca', as: :busca_produto
   root 'produtos#index'
 end
